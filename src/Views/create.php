038f67f9986447d5f6dd6063eb93fc0319bae8d7
UxerDevCoding <omarheviaarbana@gmail.com>
2022-01-12 20:11:46 +0100

    web folder and style.css file added. Further, create view was updated with a new style

--- src/Views/create.php
+++ src/Views/create.php
@@ -4,30 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="web\css\style.css">
     <title>Base MVC with PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
-    
-    <h1>Support App</h1>
-    <h3>Add a request</h3>
-    <section>
+<body>   
+        <div class="container mt-5">   
+        <h2>Support App</h2>
+        <h5 class= "badge rounded-pill bg-dark mt-2">Add a request</h5> 
         <form action="?action=store" method="post">
-            
-            <label for="topic">Topic</label>
-            <select name="topic" id="topic" required>
-                <option value="" hidden selected>Select an option</option>
-                <option value="Print error">Print error</option>
-                <option value="Windows Blue Screen">Windows Blue Screen</option>
-            </select>
-            
-            <label for="description">Description</label>
-            <textarea type="text" name="description" id="description" required></textarea>
-            
-            <label for="user_name">User name</label>
-            <input type="text" name="user_name" id="user_name" required>
-            <input type="submit" value="Add">
-        </form>        
-    </section>
-
+            <div class="col-4">
+                <label class="form-label mt-3" for="topic">Topic</label>
+                <select class="form-select" name="topic" id="topic" required>
+                    <option value="" hidden selected>Select an option</option>
+                    <option value="Print error">Print error</option>
+                    <option value="Windows Blue Screen">Windows Blue Screen</option>
+                </select></br>
+            </div>
+            <div class="col-4">
+                <label class="form-label" for="user_name">User name</label>
+                <input type="text" class="form-control" name="user_name" id="user_name" required>
+            </div></br>
+            <div class="col-4">
+                <label for="inputDescription" class="form-label">Description</label>
+                <textarea class="form-control" type="text" name="description" id="description" required></textarea></br>
+                <div class="d-grid gap-2 col-3 ">
+                <input type="submit" class="btn btn-primary" value="Add">
+            </div>
+            </div>        
+        </form> 
+    </div>   
 </body>
 </html>
